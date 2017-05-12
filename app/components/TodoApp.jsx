441a3820bc340c3ca75e61bd46e34281155f792a
Doug Stockalper <dstockalper@gmail.com>
2017-05-12 11:07:06 -0700

    Create AddTodo component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
@@ -22,14 +23,17 @@ var TodoApp = React.createClass({
           text: 'Create sweet app'
         }
       ]
-
     }
+  },
+  handleAddTodo: function(text) {
+    alert('new todo: ' + text);
   },
   render: function() {
     var {todos} = this.state;
     return (
       <div>
         <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
   }
