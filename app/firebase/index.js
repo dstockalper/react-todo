ed2d21790e70e8c2ba55f4fc913d8a2b15ac1458
Doug Stockalper <dstockalper@gmail.com>
2017-05-20 16:10:05 -0700

    fix environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -10,14 +10,14 @@ try {
     messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
-  var config = {
-    apiKey: 'AIzaSyCzUhzgM0OcCuSqUyaT19vKaB_kmNr_egA',
-    authDomain: 'todo-app-400eb.firebaseapp.com',
-    databaseURL: 'https://todo-app-400eb.firebaseio.com',
-    projectId: 'todo-app-400eb',
-    storageBucket: 'todo-app-400eb.appspot.com',
-    messagingSenderId: '215142164507'
-  };
+  // var config = {
+  //   apiKey: 'AIzaSyCzUhzgM0OcCuSqUyaT19vKaB_kmNr_egA',
+  //   authDomain: 'todo-app-400eb.firebaseapp.com',
+  //   databaseURL: 'https://todo-app-400eb.firebaseio.com',
+  //   projectId: 'todo-app-400eb',
+  //   storageBucket: 'todo-app-400eb.appspot.com',
+  //   messagingSenderId: '215142164507'
+  // };
 
   firebase.initializeApp(config);
 } catch(e) {
